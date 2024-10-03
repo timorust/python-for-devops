de758278b71391bf5d6f863558fdc65e2cbaa560
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2024-10-03 15:35:18 +0000

    adding lint

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 install:
 	pip install --upgrade pip && pip install -r requirements.txt
+lint: 
+	pylint --disable=R,C *.py devopslib
---
+++ devopslib/randomskill.py
@@ -0,0 +1,8 @@
+from random import choices
+
+def skill():
+    skills = ["fullstack", "cyber", "devops"]
+    return choices(skills)[0]
+
+#var = 1
+#var = var
